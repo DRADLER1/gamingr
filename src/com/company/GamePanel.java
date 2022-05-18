dfc8a39cb34987ed3a23729aca8db938bf913199
Shubham <dradlershubham@gmail.com>
2022-05-19 00:37:13 +0530

    part 2 and part 3 done

--- src/com/company/GamePanel.java
+++ src/com/company/GamePanel.java
@@ -1,23 +1,40 @@
 package com.company;
+import entity.Player;
+
 import javax.swing.JPanel;
-import java.awt.Color;
-import java.awt.Dimension;
+import java.awt.*;
 
 public class GamePanel extends JPanel implements Runnable {
     //  SCREEN SETTING
     final int originalTileSize =16; // 16X16 tile
     final int scale = 3;
-    final int tileSize = originalTileSize * scale; // 48X48 tile
+    public final int tileSize = originalTileSize * scale; // 48X48 tile
     final int maxScreenCol = 16;
     final int maxScreenRow = 12;
     final int screenWidth = tileSize*maxScreenCol; //768 pixel
     final int screenHieght = tileSize*maxScreenRow;
+
+    // FPS
+    int FPS = 60;
+
+
+    KeyHandler keyH = new KeyHandler();
     Thread gameThread;
+
+    //entity player
+    Player player = new Player(this,keyH);
+    // Set players default position
+    int playerX = 100;
+    int playerY = 100;
+    int playerSpeed = 4;
+
 
     public GamePanel() {
         this.setPreferredSize(new Dimension(screenWidth, screenHieght));
         this.setBackground(Color.black);
         this.setDoubleBuffered(true);
+        this.addKeyListener(keyH);
+        this.setFocusable(true);
     }
     public void startGameThread(){
         gameThread = new Thread(this);
@@ -26,12 +43,48 @@ public class GamePanel extends JPanel implements Runnable {
 
     @Override
     public void run() {
+        // delta method to add restriction to allow the movement to be smooth
+        double drawInterval = 1000000000 /FPS;
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
+        // diplays fps
+        long timer = 0;
+        int drawCount = 0;
         while (gameThread != null){
-//            System.out.println("The game loop is running");
-            // 1. UPDATE:  update information such as character position
-            // 2. DRAW: draw the screen with the updated information
+            currentTime = System.nanoTime();
+            delta += (currentTime - lastTime) /drawInterval;
+            timer += (currentTime-lastTime);
+            lastTime = currentTime;
+            if(delta >=1){
+
+                // 1. UPDATE:  update information such as character position
+                update();
+                // 2. DRAW: draw the screen with the updated information
+                repaint();
+                delta--;
+                drawCount++;
+            }
+            if (timer >= 1000000000){
+                System.out.println("FPS: " + drawCount);
+                drawCount = 0;
+                timer = 0;
+            }
+
+
+
         }
 
     }
+    public void update(){
+     player.update();
+
+    }
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D)g;
+        player.draw(g2);
+
+        g2.dispose();
+    }
 }
-//enjoy your life
